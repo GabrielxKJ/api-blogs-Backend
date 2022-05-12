02c2cd2833cde64a234f7395ba0ce1a726e1fbec
User <gabrielkleber006@gmail.com>
2022-05-12 10:48:16 -0300

    fix req 1 token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,15 @@
+const { getToken } = require('../auth/token');
 const userService = require('../services/userService');
 
 const userPost = async (req, res) => {
   const { displayName, email, password, image } = req.body;
-  const createUser = await userService.createUser(displayName, email, password, image);
-  res.status(201).json(createUser);
+  await userService.createUser(displayName, email, password, image);
+  const token = getToken(email, password);
+  res.status(201).json(token);
 };
 
 const getUser = async (_req, res) => {
   const allUsers = await userService.getUser();
-  console.log(allUsers);
   res.status(200).json(allUsers);
 };
 
