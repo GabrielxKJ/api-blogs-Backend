259d724559985bd3008b89b132c31f7e6a4c9823
User <gabrielkleber006@gmail.com>
2022-05-12 12:02:57 -0300

    fix config database

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 const m = require('../middlewares/validateUser');
-const { userPost, getUser } = require('../controllers/userController');
-
+const { userPost } = require('../controllers/userController');
+// getUser
 router.post('/', m.emailAlreadyExist, m.emailRegx, m.validatePassword, m.validateDisplay, userPost);
-router.get('/', getUser);
+// router.get('/', getUser);
 
 module.exports = router;
